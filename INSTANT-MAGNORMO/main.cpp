fed26a7f38d437292e639bf68eaca60cdb71aab1
Chris Branch <chrisb@clickteam.com>
2010-10-16 23:05:36 +0100

    New conversation windows work again

--- INSTANT-MAGNORMO/main.cpp
+++ INSTANT-MAGNORMO/main.cpp
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+	qRegisterMetaType<Contact>("Contact");
     qRegisterMetaType<QSharedPointer<Contact> >("QSharedPointer<Contact>");
 
     QCoreApplication::setOrganizationName("MAGNORMO");
