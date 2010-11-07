5d133cc6719dd5c0ca7e0d741f61121f11ea8e3f
Chris Branch <chrisb@clickteam.com>
2010-11-07 20:53:51 +0000

    Added example code (commented out) suggesting where we go next

--- INSTANT-MAGNORMO/main.cpp
+++ INSTANT-MAGNORMO/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include "MAGNORMOBOT.h"
 #include "Contact.h"
+#include "QAppBar.h"
 
 int main(int argc, char *argv[])
 {
@@ -16,9 +17,15 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain("joshfyne.com");
     QCoreApplication::setApplicationName("INSTANT_MAGNORMO");
 
-
     MainWindow w;
     w.startAccountManager(false);
     w.show();
+	/*
+	// This code shows how to put the main window into a right-docked app bar
+	QAppBar sample(QAppBarEdgeRight, 250);
+	sample.setWindowTitle("INSTANT MAGNORMO");
+	sample.getContentArea()->addWidget(&w);
+	sample.show();
+	*/
     return a.exec();
 }
